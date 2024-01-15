4609da25f0c617dbd8fc357b9687d124630e545e
Soenke J. Peters <peters__git@opcenter.de>
2024-01-15 14:34:53 +0100

    some additions

--- json2channelcsv.sh
+++ json2channelcsv.sh
@@ -4,5 +4,5 @@
 JSONFILE="7200_eeprom_decode.json"
 CSVFILE="7200_channels.csv"
 echo "type,id,name,bf1,bf2,bf3,bf" > ${CSVFILE}
-jq -r '.channels_dsc[] | [.type, .id, .name, .bf1, .bf2, .bf3, .bf] | @csv' ${JSONFILE} >> ${CSVFILE}
-jq -r '.channels_atis[] | [.type, .id, .name, .bf1, .bf2, .bf3, .bf] | @csv' ${JSONFILE} >> ${CSVFILE}
+jq -r '.channels_dsc[] | [.type, .id, .name, .bf1, .tunerx, .tunetx, .bf] | @csv' ${JSONFILE} >> ${CSVFILE}
+jq -r '.channels_atis[] | [.type, .id, .name, .bf1, .tunerx, .tunetx, .bf] | @csv' ${JSONFILE} >> ${CSVFILE}
